867576e3528e4afeab707116956105b127556c37
t.stamma <t.stamma@journaway.com>
2023-02-17 15:35:11 +0100

    add socket example

--- JaKleingartenParadies/Program.cs
+++ JaKleingartenParadies/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Runtime.Loader;
+using SocketIOClient;
+using SocketIOClient.Transport;
+
+const string Secret = "";
+
+var client = new SocketIO("https://games.uhno.de", new SocketIOOptions() {Transport = TransportProtocol.WebSocket});
+
+client.OnConnected += async (sender, e) =>
+{
+    await client.EmitAsync("authenticate", (success) =>
+    {
+
+    }, Secret);
+};
+
+client.On("disconnect", (e) => { });
+
+
+
+
 Console.WriteLine("Hello, World!");
+
