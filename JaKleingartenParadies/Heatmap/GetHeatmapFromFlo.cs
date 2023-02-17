2f9ed034f73ff5091afc7811fecb079f5b34bc7f
Kurt Sap <ksap@neusta.de>
2023-02-17 17:22:03 +0100

    kurts zeugs

---
+++ JaKleingartenParadies/Heatmap/GetHeatmapFromFlo.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace JaKleingartenParadies.Heatmap;
+
+//schicke: das grid so wie ist gerade ist, verbleibende schiffe deren schiffslänge
+public class GetHeatmapFromFlo
+{
+    private readonly HttpClient _client;
+    
+    public GetHeatmapFromFlo()
+    {
+        _client = new HttpClient();
+    }
+    
+    //von flo kommt eine list<list<float>>mit der heatmap
+    //post request
+    //json format
+    public async Task GetHeatmap(List<List<string>> grid)
+    {
+        string route = "";
+        var gridJson = JsonSerializer.Serialize(grid);
+
+        //response = probability map
+        //10 * 10 f list
+        var response = await _client.PostAsJsonAsync(route, gridJson);
+    }
+}
---
+++ JaKleingartenParadies/Heatmap/Grid.cs
@@ -0,0 +1,34 @@
+﻿namespace JaKleingartenParadies.Heatmap;
+
+public class Grid
+{
+    public Grid()
+    {
+        
+    }
+
+    /// <summary>
+    /// U = unknown
+    /// H = hit
+    /// M = miss
+    /// W = besigtes schiff
+    /// </summary>
+    /// <returns></returns>
+    public List<List<string>> GetStartGrid()
+    {
+        List<List<string>> grid = new List<List<string>>()
+        {
+            new List<string>(){"U","U", "U", "U", "U", "U", "U", "U", "U", "U"},
+            new List<string>(){"U","U", "U", "U", "U", "U", "U", "U", "U", "U"},
+            new List<string>(){"U","U", "U", "U", "U", "U", "U", "U", "U", "U"},
+            new List<string>(){"U","U", "U", "U", "U", "U", "U", "U", "U", "U"},
+            new List<string>(){"U","U", "U", "U", "U", "U", "U", "U", "U", "U"},
+            new List<string>(){"U","U", "U", "U", "U", "U", "U", "U", "U", "U"},
+            new List<string>(){"U","U", "U", "U", "U", "U", "U", "U", "U", "U"},
+            new List<string>(){"U","U", "U", "U", "U", "U", "U", "U", "U", "U"},
+            new List<string>(){"U","U", "U", "U", "U", "U", "U", "U", "U", "U"},
+            new List<string>(){"U","U", "U", "U", "U", "U", "U", "U", "U", "U"},
+        };
+        return grid;
+    }
+}
