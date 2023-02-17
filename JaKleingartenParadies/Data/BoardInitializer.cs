79bbf1f4ba8d26acee46b82d6c559a1af3f93999
okooijman <o.kooijman@journaway.com>
2023-02-17 19:44:24 +0100

    Added Start Position parsing

---
+++ JaKleingartenParadies/Data/BoardInitializer.cs
@@ -0,0 +1,59 @@
+﻿#nullable enable
+using System.Text;
+using System.Text.Json;
+using JaKleingartenParadies.Dto;
+
+namespace JaKleingartenParadies.Data;
+
+public class BoardInitializer : IAsyncDisposable
+{
+    private static readonly JsonSerializerOptions Options = new(JsonSerializerDefaults.Web); 
+    private readonly Stream _stream;
+    public BoardInitializer()
+    {
+        try
+        {
+            _stream = File.OpenRead("Data/test-init.json");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+            _stream = new MemoryStream(Encoding.UTF8.GetBytes("[]"));
+        }
+    }
+
+    public async Task<InitialStartShips[]> GetRandomStartBoardAsync()
+    {
+        try
+        {
+            JsonSerializer.Serialize(true, Options);
+            var boardInits = JsonSerializer.DeserializeAsyncEnumerable<InitialStartShips[]>(_stream, Options);
+            int randomIndex = Random.Shared.Next(6);
+            var counter = 0;
+            await foreach (var boardInitDtos in boardInits)
+            {
+                if (counter < randomIndex)
+                {
+                    counter++;
+                    continue;
+                }
+                return boardInitDtos!;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        
+
+        return Array.Empty<InitialStartShips>();
+    }
+    
+    
+
+    public ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        return _stream.DisposeAsync();
+    }
+}
---
+++ JaKleingartenParadies/Dto/InitialStartShips.cs
@@ -0,0 +1,9 @@
+﻿#nullable enable
+namespace JaKleingartenParadies.Dto;
+
+public class InitialStartShips
+{
+    public int[] Start { get; set; } = Array.Empty<int>();
+    public string Direction { get; set; } = string.Empty;
+    public int Size { get; set; }
+}
