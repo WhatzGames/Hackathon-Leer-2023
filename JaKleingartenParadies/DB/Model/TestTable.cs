fe9fb15e810cc8364553e69a5e02f415ecc87fb7
t.stamma <t.stamma@journaway.com>
2023-02-17 15:21:57 +0100

    add migration

--- JaKleingartenParadies/DB/Model/TestTable.cs
+++ JaKleingartenParadies/DB/Model/TestTable.cs
@@ -7,5 +7,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 [Table("TestTable")]
 public class TestTable
 {
-    
+    public long Uid { get; set; }
+    public string Test1 { get; set; }
+    public string Test2 { get; set; }
+    public string Test3 { get; set; }
+    public string Test4 { get; set; }
+    public string Test5 { get; set; }
 }
