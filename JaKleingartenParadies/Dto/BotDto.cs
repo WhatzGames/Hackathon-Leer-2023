8dc560985d0b2f746a560fe974ce74c33f99e3ea
t.stamma <t.stamma@journaway.com>
2023-02-17 16:21:38 +0100

    add bot-Dto, add Client functions

---
+++ JaKleingartenParadies/Dto/BotDto.cs
@@ -0,0 +1,25 @@
+﻿namespace JaKleingartenParadies.Dto;
+
+public class BotDto
+{
+    public string id { get; set; }
+    public Players[] players { get; set; }
+    public string[][] boards { get; set; }
+    public Log[] log { get; set; }
+    public string type { get; set; }
+    public string self { get; set; }
+}
+
+public class Players
+{
+    public string id { get; set; }
+    public string symbol { get; set; }
+    public int score { get; set; }
+}
+
+public class Log
+{
+    public string player { get; set; }
+    public int[] move { get; set; }
+}
+
--- JaKleingartenParadies/Program.cs
+++ JaKleingartenParadies/Program.cs
@@ -1,25 +1,80 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Runtime.Loader;
+using System.Text.Json;
+using JaKleingartenParadies.Dto;
 using SocketIOClient;
 using SocketIOClient.Transport;
 
 const string Secret = "2d376eb7-ead4-4b7c-99c0-3a21515e8cd5";
+bool gotDisconnected = false;
 
 var client = new SocketIO("https://games.uhno.de", new SocketIOOptions() {Transport = TransportProtocol.WebSocket});
 
-client.OnConnected += async (sender, e) =>
-{
-    await client.EmitAsync("authenticate", (success) =>
+client.On("connect",
+    async (e) =>
     {
+        await client.EmitAsync("authenticate",
+            (success) => { Console.WriteLine($"Authentication successful: {success}"); }, Secret);
+    });
 
-    }, Secret);
-};
+client.On("disconnect", (e) =>
+{
+    Console.WriteLine("Disconnected");
+    gotDisconnected = true;
+});
 
-client.On("disconnect", (e) => { });
+
+await client.ConnectAsync();
+
+client.On("data", (response) =>
+{
+    var data = JsonSerializer.Deserialize<BotDto>(response.GetValue()
+                                                          .GetString());
+    switch (data.type)
+    {
+        case "INIT":
+            Init(data);
+
+            break;
+        case "RESULT":
+            Result(data);
+
+            break;
+
+        case "SET":
+            Set(data, response);
+            break;
+        case "ROUND":
+            Round(data, response);
+            break;
+    }
+});
 
 
+while (!gotDisconnected)
+{
+    await Task.Delay(10000);
+    Console.WriteLine("Still alive!");
+}
 
 
-Console.WriteLine("Hello, World!");
+void Init(BotDto botDto)
+{
+    throw new NotImplementedException();
+}
 
+
+void Result(BotDto botDto)
+{
+    throw new NotImplementedException();
+}
+void Set(BotDto botDto, SocketIOResponse socketIoResponse)
+{
+    throw new NotImplementedException();
+}
+
+
+void Round(BotDto botDto, SocketIOResponse socketIoResponse)
+{
+    throw new NotImplementedException();
+}
